eb8d33b1fbd54acce72255f5ca72c017308b44ae
Mapetr <info@mapetr.moe>
2025-03-17 10:08:50 +0100

    merge 'main' into 'shortener'

--- lib/gifConvert.tsx
+++ lib/gifConvert.tsx
@@ -1,12 +1,23 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
+import posthog from "posthog-js";
 
 export async function ConvertToGif(ffmpeg: FFmpeg, file: File): Promise<File> {
   const outputFileName = `${file.name.replace(/\.[^/.]+$/, "")}.gif`;
 
+  const start = Date.now();
+
   await ffmpeg.writeFile(file.name, await fetchFile(file));
   await ffmpeg.exec(['-i', file.name, "-vf", "fps=30,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256[p];[s1][p]paletteuse=dither=bayer:bayer_scale=5", "-loop", "0", outputFileName]);
   const data = (await ffmpeg.readFile(outputFileName));
+
+  const timeTaken = Date.now() - start;
+
+  posthog.capture("gifConvert", {
+    fileSize: file.size,
+    fileType: file.type,
+    timeTaken: timeTaken
+  });
 
   return new File([data], outputFileName, {type: "image/gif"});
 }
