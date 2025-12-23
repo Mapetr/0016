431e5e75aec002c5bb1e60ece40697008767cc21
mapetr <info@mapetr.moe>
2025-12-23 01:57:38 +0100

    change checkboxlabel behaviour

--- components/CheckboxLabel.tsx
+++ components/CheckboxLabel.tsx
@@ -2,16 +2,38 @@ import { Label } from "@/components/ui/label";
 import React from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 
-export default function CheckboxLabel({ text, setChecked, disabled = false }: {
-  text: string,
-  setChecked: React.Dispatch<React.SetStateAction<boolean>>,
-  disabled: boolean
+export default function CheckboxLabel({
+                                        text,
+                                        checked,
+                                        setChecked,
+                                        disabled = false
+                                      }: {
+  text: string;
+  checked: boolean;
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
+  disabled: boolean;
 }) {
+  const handleClick = () => {
+    if (!disabled) {
+      setChecked((prev) => !prev);
+    }
+  };
+
   return (
-    <div className="flex items-center space-x-2">
-      <Checkbox id={"gif"} onCheckedChange={(checked) => setChecked(checked === "indeterminate" ? false : checked)}
-                disabled={disabled} />
-      <Label htmlFor={"gif"}>{text}</Label>
+    <div
+      className="flex items-center space-x-2 cursor-pointer"
+      role="button"
+      tabIndex={0}
+    >
+      <Checkbox
+        id={"gif"}
+        checked={checked}
+        onCheckedChange={handleClick}
+        disabled={disabled}
+      />
+      <Label onClick={handleClick} htmlFor={"gif"} className="cursor-pointer">
+        {text}
+      </Label>
     </div>
   );
 }
